b0d55653a15dc27cdffe1d0ef6e7d5aa3e3f1e3d
unknown <matheusczeckcz@gmail.com>
2026-07-03 20:49:23 -0300

    AppUrl real em prod

--- Front-end/src/app/environments/environment.prod.ts
+++ Front-end/src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://menos-link-production.up.railway.app',
-  appUrl: 'https://jolly-jalebi-3a6549.netlify.app'
+  appUrl: 'https://menoslink.matheushcz.dev'
 };
